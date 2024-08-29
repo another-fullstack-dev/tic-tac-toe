dffd2f3fae57cd14f5672c7ee8163c9e6e8ba835
draemato <97753719+draemato@users.noreply.github.com>
2024-08-29 21:19:59 +0700

    made 3 in a row win check

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const gameboard = (function() {
     const row1 = [null, null, null];
     const row2 = [null, null, null];
     const row3 = [null, null, null];
-    return {row1, row2, row3};
+    const hasNull = {};
+    return {row1, row2, row3, hasNull};
 })();
 
 function setPlayers(){
@@ -10,12 +11,46 @@ function setPlayers(){
     let p2 = {};
     p1.name = prompt("Enter a name of first player (O):");
     p2.name = prompt("Enter a name of second player (X):");
-    p1.sign = "O";
-    p2.sign = "X";
+    p1.sign = 0; // O
+    p2.sign = 1; // X
     return {p1, p2};
 };
 
-
+// may the god have mercy on my soul
+function checkWin(){
+    let win = false;
+    if (gameboard.hasNull.row1 == "null" &&
+        gameboard.hasNull.row2 == "null" &&
+        gameboard.hasNull.row3 == "null"){
+            return win = "tie";
+        }
+    for (const row in gameboard) {
+        if (row == "hasNull"){
+            return;
+        }
+        if ( gameboard.hasNull[row] == "null"){
+            continue;
+        }
+        if (gameboard.hasNull[row] == true && gameboard[row].includes(null)){
+            continue;
+        }
+        if (gameboard[row].includes(null)){
+            gameboard.hasNull[row] = true;
+            return;
+        } else {
+            gameboard.hasNull[row] = false;
+        }
+        if (gameboard.hasNull[row] == false){
+            if (gameboard[row].reduce((total, current) => total + current) == 0 ||
+                gameboard[row].reduce((total, current) => total + current) == 3){
+                return win = true
+            } else {
+                gameboard.hasNull[row] = "null";
+                return;
+            }
+        }
+    }
+}
 
 function playGame(){
     const players = setPlayers();
@@ -38,6 +73,10 @@ function playGame(){
         console.table(gameboard);
         if (checkWin()) {
             break;
+        } else if (checkWin() == "tie"){
+            console.table(gameboard);
+            console.log("It's a tie!")
+            return;
         }
     }
 
